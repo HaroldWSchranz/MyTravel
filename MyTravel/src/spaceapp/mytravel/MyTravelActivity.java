f2585351ee5359321d95c7d4236f2fbf8296aea9
Ashton Batty <aisidun@gmail.com>
2012-04-21 20:42:07 +1000

    Initial commit

---
+++ MyTravel/src/spaceapp/mytravel/MyTravelActivity.java
@@ -0,0 +1,13 @@
+package spaceapp.mytravel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyTravelActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
